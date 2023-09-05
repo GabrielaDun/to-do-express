52e7694ec32a5f27ca15c8bbefa01bd34e4786c4
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-09-05 12:38:27 +0200

    Developed server

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require('express')
 const app = express();
+const socket = require('socket.io');
 
-app.listen (8000, () => {
+const server = app.listen (8000, () => {
     console.log('Server is running on port: 8000')
 })
+const io = socket(server);
+
+let tasks = [];
+
+io.on('connection', (socket) => {
+    socket.emit('updateData', tasks)
+
+    socket.on('addTask', ( task ) => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task)
+    })
+    socket.on('removeTask', ( taskId ) => {
+        const taskIndex = tasks.findIndex(task => {
+            return task.id === socket.id
+        })
+
+        if (taskIndex !== -1) {
+            const removedTask = tasks.splice(taskDelete, 1)
+            socket.broadcast.emit('removeTask', removedTask)
+        }
+    })
+    
+})
+
+app.use((req, res) => {
+    res.status(404).send({ message: 'Not found...' });
+});
